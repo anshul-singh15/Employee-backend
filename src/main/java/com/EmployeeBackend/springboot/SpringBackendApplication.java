4774473e5f1f3c82037461e94c7e0b636783e4af
anshul-singh15 <116735359+anshul-singh15@users.noreply.github.com>
2023-07-16 16:05:20 +0530

    Update SpringBackendApplication.java

--- src/main/java/com/EmployeeBackend/springboot/SpringBackendApplication.java
+++ src/main/java/com/EmployeeBackend/springboot/SpringBackendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.EmployeeBackend.springboot.controller"})
 public class SpringBackendApplication {
 
 	public static void main(String[] args) {
